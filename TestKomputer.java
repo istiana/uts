baa5eb9ed258bc9d822798f5cc2496f9df8d07a0
Istiana <istiana.ubudiy@gmail.com>
2015-05-12 17:27:58 +0700

    utsIstiana
    type2

---
+++ TestKomputer.java
@@ -0,0 +1,10 @@
+public class TestKomputer
+{
+	public static void main(String[]args)
+	{
+		Notebook16 asus = new Notebook16(3500,4000,80000,7000)
+		asus.CetakSpesifikasi();
+		asus.Overlock(3000);
+		asus.Overlock(3200);
+	}
+}
